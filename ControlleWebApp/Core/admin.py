6d676f94d26984ebf960e62e5dbb6fa97679d860
Henrique Nascimento <nascimentocostahenrique@gmail.com>
2020-05-01 07:49:28 -0300

    Currency e outros models

--- ControlleWebApp/Core/admin.py
+++ ControlleWebApp/Core/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import lancamentoConta
+from .models import entradas
+from .models import saidas
+
+class lancamentoContaAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(lancamentoConta, admin.ModelAdmin)
+admin.site.register(entradas, admin.ModelAdmin)
+admin.site.register(saidas, admin.ModelAdmin)
--- ControlleWebApp/Core/models.py
+++ ControlleWebApp/Core/models.py
@@ -1,7 +1,29 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
+from django.contrib.auth.models import User
 
 class lancamentoConta(models.Model):
-    valor = models.IntegerField()
+    valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
     vencimento = models.DateField()
     data = models.DateTimeField(auto_now_add=True)
     lembrar = models.BooleanField()
+    usuario = models.ForeignKey(User, verbose_name="usuario", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'LANCAMENTO DE CONTAS'
+
+class entradas(models.Model):
+    valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, verbose_name="usuario", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'ENTRADAS'
+
+class saidas(models.Model):
+    valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, verbose_name="usuario", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'SAIDAS'
