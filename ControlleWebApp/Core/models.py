1124ea7ff20d33a742db8bdef7201fb7a33c52dc
Henrique Nascimento <nascimentocostahenrique@gmail.com>
2020-05-01 09:39:40 -0300

    Usuario

--- ControlleWebApp/Core/models.py
+++ ControlleWebApp/Core/models.py
@@ -6,6 +6,7 @@ class lancamentoConta(models.Model):
     valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
     vencimento = models.DateField()
     data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, verbose_name="usuario", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'LANCAMENTO DE CONTAS'
