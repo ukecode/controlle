77b8d1335b9dfb9725ff18d1bf5e0d54a2d2b4ac
Henrique Nascimento <nascimentocostahenrique@gmail.com>
2020-05-01 14:39:54 -0300

    Insercao de outros modelos

--- ControlleWebApp/Core/admin.py
+++ ControlleWebApp/Core/admin.py
@@ -2,10 +2,33 @@ from django.contrib import admin
 from .models import lancamentoConta
 from .models import entradas
 from .models import saidas
+from .models import origemsRegistros
 
 class lancamentoContaAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['valor', 'vencimento', 'forma_pagamento', 'tipo', 'status']
+   
+    #fields = ['descricao', 'valor', 'vencimento', 'tipo', 'forma_pagamento', 'status']
 
-admin.site.register(lancamentoConta, admin.ModelAdmin)
-admin.site.register(entradas, admin.ModelAdmin)
-admin.site.register(saidas, admin.ModelAdmin)
+    fieldsets = (
+        (None, {
+            'fields': ('descricao', 'valor', 'vencimento', 'tipo')
+        }),
+        ('Observacoes', {
+            'fields': ('forma_pagamento', 'status')
+        })
+    )
+
+class origemsRegistrosAdmin(admin.ModelAdmin):
+    list_display = ['titulo']
+
+class entradasAdmin(admin.ModelAdmin):
+    list_display = ['valor', 'origem_entrada']
+
+class saidasAdmin(admin.ModelAdmin):
+    list_display = ['valor', 'origem_saida']
+
+
+admin.site.register(lancamentoConta, lancamentoContaAdmin)
+admin.site.register(origemsRegistros, origemsRegistrosAdmin)
+admin.site.register(entradas, entradasAdmin)
+admin.site.register(saidas, saidasAdmin)
--- ControlleWebApp/Core/models.py
+++ ControlleWebApp/Core/models.py
@@ -2,15 +2,46 @@ from django.db import models
 from djmoney.models.fields import MoneyField
 from django.contrib.auth.models import User
 
+class origemsRegistros(models.Model):
+    titulo = models.CharField(max_length=20)
+    data = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'CATEGORIAS'
+
+    def __str__(self):
+        return self.titulo
+
 class lancamentoConta(models.Model):
+    escolha_pagamento = (
+        ('de', 'DEBITO'),
+        ('cr', 'CREDITO')
+    )
+
+    status_pagamento = (
+        ('pa', 'PAGO'),
+        ('ab', 'ABERTO')
+    )
+
+
+    descricao = models.CharField(max_length=160)
+
     valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
     vencimento = models.DateField()
+    forma_pagamento = models.CharField(max_length=2, choices=escolha_pagamento)
+    tipo = models.ForeignKey(origemsRegistros, on_delete=models.CASCADE)
     data = models.DateTimeField(auto_now_add=True)
+
+    status = models.CharField(max_length=2, choices=status_pagamento)
 
     class Meta:
         verbose_name_plural = 'LANCAMENTO DE CONTAS'
 
+    def __str__(self):
+        return self.descricao
+
 class entradas(models.Model):
+    origem_entrada = models.ForeignKey(origemsRegistros, on_delete=models.CASCADE)
     valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
     data = models.DateTimeField(auto_now_add=True)
     usuario = models.ForeignKey(User, verbose_name="usuario", on_delete=models.CASCADE)
@@ -18,10 +49,17 @@ class entradas(models.Model):
     class Meta:
         verbose_name_plural = 'ENTRADAS'
 
+    def __str__(self):
+        return '{}'.format(self.origem_entrada)
+
 class saidas(models.Model):
+    origem_saida = models.ForeignKey(origemsRegistros, on_delete=models.CASCADE)
     valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
     data = models.DateTimeField(auto_now_add=True)
     usuario = models.ForeignKey(User, verbose_name="usuario", on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = 'SAIDAS'
+
+    def __str__(self):
+        return '{}'.format(self.origem_saida)
