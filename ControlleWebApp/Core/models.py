627677644eaf3fcf0c4b36f57ce1b4c37fe250a8
Henrique Nascimento <nascimentocostahenrique@gmail.com>
2020-05-01 08:57:27 -0300

    update files

--- ControlleWebApp/Core/models.py
+++ ControlleWebApp/Core/models.py
@@ -6,7 +6,7 @@ class lancamentoConta(models.Model):
     valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
     vencimento = models.DateField()
     data = models.DateTimeField(auto_now_add=True)
-    lembrar = models.BooleanField(Blank=True)
+    lembrar = models.BooleanField()
     usuario = models.ForeignKey(User, verbose_name="usuario", on_delete=models.CASCADE)
 
     class Meta:
