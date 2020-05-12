7e3da5d3b14147ff410f7840ee942f3d474fa433
HENRIQUE <henriquemicroweb@gmail.com>
2020-05-12 16:21:57 -0300

    Adicionar pagina de login

--- ControlleWebApp/Core/views.py
+++ ControlleWebApp/Core/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.urls import reverse_lazy
 
 def paginaInicial(request):
     return render(request, 'core/home.html')
+
+def loginUsuario(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('Core:dashboard_home')
+    else:
+        return render(request, 'core/login.html')
+
+def painelUsuario(request):
+    return render(request, 'core/logged.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('Core:login')
