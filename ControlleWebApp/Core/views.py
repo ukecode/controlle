15f6cb1e2d0fa5cd6d3ec04936ddae69ab9de667
HENRIQUE <henriquemicroweb@gmail.com>
2020-05-14 17:15:36 -0300

    Tratamento de erros na tela de login

--- ControlleWebApp/Core/views.py
+++ ControlleWebApp/Core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout
 #from django.urls import reverse_lazy
 
@@ -7,14 +9,22 @@ def paginaInicial(request):
     return render(request, 'core/home.html')
 
 def loginUsuario(request):
-    username = request.POST.get("username")
-    password = request.POST.get("password")
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect('Core:dashboard_home')
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('Core:dashboard_home')
+        else:
+            messages.error(request, 'usuario ou senha invalido')
+            return redirect('Core:login')
+            
+
     else:
-        return render(request, 'core/login.html')
+        form = AuthenticationForm()
+        return render(request, 'core/login.html', {'form': form})
 
 def painelUsuario(request):
     return render(request, 'core/logged.html')
