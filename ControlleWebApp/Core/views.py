2523f70971293c4b8d4b7b9967f65fc6aa6e2d80
HENRIQUE <henriquemicroweb@gmail.com>
2020-05-15 08:21:12 -0300

    Modificacoes no sistema de login

--- ControlleWebApp/Core/views.py
+++ ControlleWebApp/Core/views.py
@@ -1,29 +1,29 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
+from .forms import LoginForm
 from django.contrib.auth import logout
-#from django.urls import reverse_lazy
 
 def paginaInicial(request):
     return render(request, 'core/home.html')
 
 def loginUsuario(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
+        form = LoginForm(request.POST)
         username = request.POST.get("username")
         password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
+
         if user is not None:
             login(request, user)
             return redirect('Core:dashboard_home')
+
         else:
             messages.error(request, 'usuario ou senha invalido')
             return redirect('Core:login')
             
-
     else:
-        form = AuthenticationForm()
+        form = LoginForm
         return render(request, 'core/login.html', {'form': form})
 
 def painelUsuario(request):
